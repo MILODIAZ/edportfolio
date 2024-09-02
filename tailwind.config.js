1e8ecea6fdd67f0f59627be01cbfa051780af354
MILODIAZ <milodiaz1995@gmail.com>
2024-09-02 04:04:01 -0400

    seccion de skills y proyectos lista

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ module.exports = {
         },
         primary: "#3758F9",
         "blue-dark": "#1B44C8",
-        secondary: "#13C296",
+        secondary: "#e706b6",
         "body-color": "#637381",
         "body-secondary": "#8899A8",
         warning: "#FBBF24",
