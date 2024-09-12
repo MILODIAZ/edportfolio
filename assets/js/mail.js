f989bf8b0f96d9a707b4d930c330d87a478f9d2b
MILODIAZ <milodiaz1995@gmail.com>
2024-09-12 18:33:10 -0300

    smtpjs configuration

---
+++ assets/js/mail.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('contactForm');
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "emilio.diaz@alumnos.ucn.cl",
+        Password: "4C6E46CE22E4B133D1630B29C6F0BBD0B029",
+        To: 'emilio.diaz@alumnos.ucn.cl',
+        From: "emilio.diaz@alumnos.ucn.cl",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    sendEmail();
+});
